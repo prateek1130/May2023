ed47d371a88ed2d566ba8227da04d70ea9ec5536
Prateek Shrivastav <106696756+prateek1130@users.noreply.github.com>
2023-06-24 00:05:03 +0530

    Challenge-2 Solution.c

--- Challenge-2 Solution.c
+++ Challenge-2 Solution.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-typedef struct my_date_t
-{
+typedef struct my_date_t {
     uint8_t date;
     uint8_t month;
     uint16_t year;
 } my_date_t;
 
-int parseDateString(const char* dateString, my_date_t* result) {
-    if (dateString == NULL || result == NULL || strlen(dateString) != 10)
-        return -1;
+typedef enum status_t {
+    SUCCESS,
+    NULL_PTR,
+    INCORRECT
+} status_t;
 
-    char dayStr[3], monthStr[3], yearStr[5];
-    sscanf(dateString, "%2[^/]/%2[^/]/%4s", dayStr, monthStr, yearStr);
-    
+status_t convertStringToDate(const char* dateStr, my_date_t* dateStruct) {
+    if (dateStr == NULL || dateStruct == NULL) {
+        return NULL_PTR;
+    }
 
-    int day = atoi(dayStr);
-    int month = atoi(monthStr);
-    int year = atoi(yearStr);
+    // Check if the date string has the correct format "DD/MM/YYYY"
+    if (sscanf(dateStr, "%hhu/%hhu/%hu", &dateStruct->date, &dateStruct->month, &dateStruct->year) != 3) {
+        return INCORRECT;
+    }
 
-    result->date = (uint8_t)day;
-    result->month = (uint8_t)month;
-    result->year = (uint16_t)year;
-    
-    return 0;
+    // Additional checks for valid values
+    if (dateStruct->date < 1 || dateStruct->date > 31 || dateStruct->month < 1 || dateStruct->month > 12 || dateStruct->year < 1) {
+        return INCORRECT;
+    }
+
+    return SUCCESS;
 }
 
 int main() {
-     char dateString[12];
-    fgets(dateString, sizeof(dateString), stdin);
-    my_date_t result;
-    
-    if (0==parseDateString(dateString, &result)) {
-        printf("Date: %02u/%02u/%04u\n", result.date, result.month, result.year);
-    } else {
-        printf("Failed to parse date string.\n");
+    const char* dateStr = "07/06/2023";
+    my_date_t dateStruct;
+    status_t status = convertStringToDate(dateStr, &dateStruct);
+
+    if (status == SUCCESS) {
+        printf("Conversion successful: %02d/%02d/%04d\n", dateStruct.date, dateStruct.month, dateStruct.year);
+    } else if (status == NULL_PTR) {
+        printf("Error: NULL pointers.\n");
+    } else if (status == INCORRECT) {
+        printf("Error: Incorrect values for date, month, or year.\n");
     }
-    
+
     return 0;
 }
